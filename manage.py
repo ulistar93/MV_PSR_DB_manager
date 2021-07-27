c3ad02e546efc5ace710b4c24b92cd926740d7f3
Changmin Yi <ycm93dg@gmail.com>
2021-07-27 17:24:06 +0900

    [feat] add prototype
    TODO

---
+++ manage.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import pandas
+import pickle # vs json
+import json
+
+class DB_tsr():
+  def __init__:
+    pass
+
+  def load_pickle(self):
+    pass
+
+  def read_all_dir(self, source_dir):
+    pass
+
+  def set_new_dest_dir(self, new_dest):
+    pass
+
+  def integrate(self):
+    pass
+
+  def save_coco(self):
+    pass
+
+  def save_yolo(self):
+    pass
+
+  def filter(self, keyword):
+    pass
+
+  def exclude(self, keyword):
+    pass
+
+if __name__ == "__main__":
+  if len(sys.argv) > 1:
+    print("sys.argv > 1 -> do sth")
+  else:
+    print("[error] no argument")
+  pass
