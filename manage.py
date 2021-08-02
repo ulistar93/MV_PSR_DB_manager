fdfd67e398e1fe7c59a6f99f42c87c760c15fd93
Changmin Yi <ycm93dg@gmail.com>
2021-08-02 20:21:37 +0900

    [feat] migrate func psuedo work 1
    - sdir, tdir existance check
    - path exception handle
    - load db_table.pkl in s_dir
    
    TODO
    - merge the several project and tasks as a single tdir TSR
    - catgories comparison is working on

--- manage.py
+++ manage.py
@@ -19,7 +19,7 @@ class Task:
     self.shortname, self.image_loc, self.image_files, self.anno_file = self.set_task_info(t_path)
 
   def __repr__(self):
-    return "name: %s\nshortname: %s\npath: %s\n# of images: %d\nimage_location: %s\nanno_file: %s" % (self.name,self.shortname,str(self.path),len(self.image_files),str(self.image_loc),str(self.anno_file))
+    return "{name: %s, shortname: %s, path: %s, # of images: %d, image_location: %s, anno_file: %s}" % (self.name,self.shortname,str(self.path),len(self.image_files),str(self.image_loc),str(self.anno_file))
     #return "name: %s\nshortname: %s\npath: %s\nimage_location: %s\n# of images: %d\nanno_file: %s" % (self.name,self.shortname,str(self.path),str(self.image_loc),len(self.image_files),str(self.anno_file))
 
   def set_task_info(self, path):
@@ -73,52 +73,17 @@ class Project:
     self.task_list = [ Task(x) for x in list(p_path.iterdir()) if x.is_dir() ]
 
   def __repr__(self):
-    return "name: %s\nlen(task_list): %d" % (self.name,len(self.task_list))
+    return "{name: %s, len(task_list): %d}" % (self.name,len(self.task_list))
 
 class TSR:
   def __init__(self, sdir):
     self.name = "TSR"
-    self.path = ''
+    self.path = str(sdir)
     self.date_created = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     self.plist = self.set_project_list(sdir)
-    _table_file = self.check_table_exist()
-    if _table_file:
-      self.read_table_pickle(_table_file)
-    else:
-      self.read_table_dir(sdir)
-    #table = projects[]
-    pass
 
   def __repr__(self):
     return "name: %s\ndate_created: %s\nlen(plist): %d" % (self.name,self.date_created,len(self.plist))
-
-  def check_table_exist(self, sdir):
-     # saved pickle check
-    pk = Path(s_dir) / 'mv_table.pkl'
-    #p = Path(s_dir) / 'mv_table.json'
-    return pk if Path.exists(pk) else ''
-
-  def read_table_picke(self, table_file):
-    # saved pickle check
-    pk = Path(table_dir) / 'table_dir.pkl'
-    #p = Path(table_dir) / 'table_dir.json'
-    #pdb.set_trace()
-    with open(pk, 'rb') as f:
-      #tsr_table = dill.load(f) # load TSR class
-      tsr_table = pickle.load(f) # load TSR class
-      #tsr_table = json.load(f) # load TSR class
-    #else:
-    #  tsr_table = TSR(table_dir)
-    #  with open(pk, 'wb') as f:
-        #dill.dump(tsr_table, f) # save TSR class
-    #    pickle.dump(tsr_table, f) # save TSR class
-        #json.dump(tsr_table, f, indent=4) # save TSR class # TODO - to be serialized
-
-    #json
-#  def __dict__(self):
-#    return { "name" : self.name,
-#            "date_created" : self.date_created,
-#            "plist" : self.plist }
 
   def set_project_list(self, sdir):
     _plist = []
@@ -127,25 +92,6 @@ class TSR:
       _plist.append(Project(d))
     #pdb.set_trace()
     return _plist
-
-def load_dataset(db_dir):
-  # saved pickle check
-  pk = Path(db_dir) / 'table_dir.pkl'
-  #p = Path(db_dir) / 'table_dir.json'
-  #pdb.set_trace()
-  tsr_db = ""
-  if Path.exists(pk):
-    with open(pk, 'rb') as f:
-      #tsr_table = dill.load(f) # load TSR class
-      tsr_table = pickle.load(f) # load TSR class
-      #tsr_table = json.load(f) # load TSR class
-  else:
-    tsr_table = TSR(table_dir)
-    with open(pk, 'wb') as f:
-      #dill.dump(tsr_table, f) # save TSR class
-      pickle.dump(tsr_table, f) # save TSR class
-      #json.dump(tsr_table, f, indent=4) # save TSR class # TODO - to be serialized
-  return tsr_table
 
 def override_dataset(tsr_old, tsr_new):
   # copy tsr_new only if the destination file is not exist in tsr_old
@@ -192,21 +138,78 @@ def migrate(s_dir, t_dir):
     - return new tsr class for sanity check
   '''
   print("do migrate")
-  pdb.set_trace()
-  pass
-  #t_dir empty check
 
-  #s_dir table check
+  #t_dir empty check
+  tp = Path(t_dir)
+  if not tp.exists():
+    print("* the t_dir %s is not exist -> make a new dir *" % t_dir)
+    tp.mkdir()
+  elif not tp.is_dir():
+    print("* the t_dir %s exist but not a dir -> abort *" % t_dir)
+    return None
+  elif len(list(tp.iterdir())) > 0:
+    #tdir is alread exist and not empty
+    print("* the t_dir %s exist but not emtpy -> please use \"update\" *" % t_dir)
+    return None
+
+  #s_dir exist check
+  sp = Path(s_dir)
+  if not sp.exists():
+    print("* the s_dir %s is not exist -> abort *" % s_dir)
+    return None
+
+  #s_dir table file check
+  st_pkl = Path(s_dir) / "db_table.pkl"
+  if st_pkl.exists():
     #s_dir table load
+    print("* s_dir db_table.pkl file already exist -> load db_table.pkl *")
+    with open(st_pkl, 'rb') as f:
+      tsr_table = pickle.load(f) # load TSR class
+      #tsr_table = json.load(f) # load TSR class
+      #tsr_table = dill.load(f) # load TSR class
+  else:
     #s_dir table create
-      #s_dir table save
+    print("* s_dir db_table.pkl file is not exist -> make new TSR & save pkl")
+    tsr_table = TSR(Path(s_dir))
+    #s_dir table save
+    with open(st_pkl, 'wb') as f:
+      pickle.dump(tsr_table, f) # save TSR class
+      #json.dump(tsr_table, f, indent=4) # save TSR class # TODO - to be serialized
+      #dill.dump(tsr_table, f) # save TSR class
 
   # copy and generate t_dir dataset #
+  print("do copy and gen tdir")
+  pdb.set_trace()
+  #tp = Path(t_dir) #done above
+  tp_task = tp / "project_0" / "task_0"
+  tp_anno = tp_task / "annotations"
+  tp_image = tp_task / "images"
+  common_cat = {}
+  for p in tsr_table.plist:
+    for t in p.task_list:
+      task_mapping_info = {}
+      task_mapping_info['org_task_name'] = t.name
+      rname= t.shortname
+      img_loc = Path(t.image_location)
+      with open(t.anno_file, 'r') as f:
+        anno = json.load(f)
+        # dummy info -> do nothing, later
+        # category check
+        if bool(common_cat):
+          common_cat = anno['categories']
+        catmap = # TODO
+        # licneses stacking
+        # image mapping
+        for img in anno['images']:
+          img['id']
+        # annotations remapping
 
   #make new tsr
+  print("make new tsr")
     #? dir read만 하면 되는거 아닌가?
     #save table in t_dir
   #return new tsr
+  return tsr_table
 
 def update():
   '''
@@ -247,12 +250,22 @@ if __name__ == "__main__":
       if (ans == 'y') or (ans == 'Y') or (ans == ''):
         args.sdir = "./results"
         print("* sdir = %s *" % args.sdir)
+      else:
+        print("* sdir is not decided -> abort *")
+        exit(0)
     if args.tdir == None:
-      ans = input("command \"migrate\" require --tdir. continue as default? [default=./] [Y/n]: ")
+      ans = input("command \"migrate\" require --tdir. continue as default? [default=./tsr] [Y/n]: ")
+
       if (ans == 'y') or (ans == 'Y') or (ans == ''):
-        args.tdir = "./"
+        args.tdir = "./tsr"
         print("* tdir = %s *" % args.tdir)
-    migrate(args.sdir, args.tdir) # do sth
+      else:
+        print("* sdir is not decided -> abort *")
+        exit(0)
+    ntsr = migrate(args.sdir, args.tdir)
+    if ntsr == None:
+      print("* the migration ended abnormally, please refer the error code *")
+      exit(0)
   elif args.command == "update":
     # option --tdir check
     update() # do sth
@@ -261,37 +274,3 @@ if __name__ == "__main__":
     extract() # do sth
   else:
     args.__repr__()
-
-  '''
-  print("#argv=%d"%len(sys.argv))
-  pdb.set_trace()
-  if len(sys.argv) > 1:
-    s_dir = path(sys.argv[1].strip())
-    if not s_dir.exists():
-      print("** %s is not exist **" % s_dir)
-      pdb.set_trace()
-      exit(0)
-    t_dir = path(sys.argv[2].strip())
-    override = false
-    if t_dir.exists():
-      print("** %s is already exist **" % t_dir)
-      print("** do you want to override? [y/n]: ", end='')
-      ans = input()
-      if ans is 'y':
-        print("** continued (do override) **")
-        override = true
-        pass
-      else:
-        print("** aborted **")
-        exit(0)
-    tsr = load_dataset(s_dir)
-    tsr_new = integrate_dataset(tsr,t_dir,override=override)
-    pdb.set_trace()
-  else:
-    print("[Usage] ./manage.py command [args...]")
-    print()
-    print("  (commands)")
-    print(migrate.__doc__)
-    print(update.__doc__)
-    print(filter.__doc__)
-  '''
