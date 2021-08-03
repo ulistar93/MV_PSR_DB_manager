b53edb440151f47797315f8565a7b1f39c5f3494
Changmin Yi <ycm93dg@gmail.com>
2021-08-03 18:29:25 +0900

    [feat] migrate func implementation 1
    TODO - fix shutil's org_file location

--- manage.py
+++ manage.py
@@ -10,7 +10,31 @@ import json
 from pathlib import Path
 import datetime
 import re
+import shutil
 import pdb
+
+def yntest(message, default):
+  '''
+    message : display text
+    default : string with [y/n] choices
+  '''
+  redo_msg = ''
+  while True:
+    ans = input(redo_msg + message + ' ' + default + ': ')
+    if ('y' in ans) or ('Y' in ans):
+      return True
+    elif ('n' in ans) or ('N' in ans):
+      return False
+    elif ans == '':
+      if 'Y' in default:
+        return True
+      elif 'N' in default:
+        return False
+      else:
+        print("** There is no default option. Please retry **")
+    else:
+      print("** There is wrong input. Please retry **")
+    redo_msg = 're: '
 
 class Task:
   def __init__(self, t_path):
@@ -145,7 +169,7 @@ def migrate(s_dir, t_dir):
     print("* the t_dir %s is not exist -> make a new dir *" % t_dir)
     tp.mkdir()
   elif not tp.is_dir():
-    print("* the t_dir %s exist but not a dir -> abort *" % t_dir)
+    print("** the t_dir %s exist but not a dir -> abort **" % t_dir)
     return None
   elif len(list(tp.iterdir())) > 0:
     #tdir is alread exist and not empty
@@ -155,7 +179,7 @@ def migrate(s_dir, t_dir):
   #s_dir exist check
   sp = Path(s_dir)
   if not sp.exists():
-    print("* the s_dir %s is not exist -> abort *" % s_dir)
+    print("** the s_dir %s is not exist -> abort **" % s_dir)
     return None
 
   #s_dir table file check
@@ -179,30 +203,148 @@ def migrate(s_dir, t_dir):
 
   # copy and generate t_dir dataset #
   print("do copy and gen tdir")
+  # TODO(changmin) : making as a function of some class object would be botter...
+  #                  candidate = TSR? or else?
+  def categories_cmp(cm, an):
+    '''
+    return dictionary which id(int) to id(int)
+    '''
+    res_dict = {}
+    info_dict = []
+    for cat_an in an:
+      _name = cat_an['name']
+      s_id = cat_an['id']
+      t_id = -1
+      for cat_cm in cm:
+        if _name == cat_cm['name']:
+          t_id = cat_cm['id']
+          break
+      if t_id == -1:
+        if yntest("* the task has more categories than original one *\
+                  * do you want to expand categories set and continue the migration? *",
+                  "[y/N]"):
+            continue
+        else:
+          return None, None
+      res_dict[s_id] = t_id
+      info_dict.append({"name":_name, "id_change":"%d->%d"%(s_id, t_id)})
+    return res_dict, info_dict
+
   pdb.set_trace()
   #tp = Path(t_dir) #done above
-  tp_task = tp / "project_0" / "task_0"
-  tp_anno = tp_task / "annotations"
-  tp_image = tp_task / "images"
+  #tp_task = tp / "project_0" / "task_0"
+  tp_pj_task_name = "project_0/task_0"
+  #tp_anno = tp_task / "annotations"
+  #tp_image = tp_task / "images"
   common_cat = {}
+  new_anno_json = {}
+  new_anno_json['images'] = []
+  new_anno_json['annotations'] = []
+
+  g_img_id = 0
+  # for project
+  #   for task
+  #     0) categories check
+  #     1) copy images
+  #     2) make annotation detail
+  all_migration_info = {} # {project{task[cat_id_map, img_id_map]}}
   for p in tsr_table.plist:
+    project_mapping_info = {}
+    project_mapping_info['name'] = p.name
     for t in p.task_list:
       task_mapping_info = {}
       task_mapping_info['org_task_name'] = t.name
+      task_mapping_info['cat_id_map'] = []
+      task_mapping_info['img_id_map'] = []
       rname= t.shortname
-      img_loc = Path(t.image_location)
+      # anno_file(instances_default.json) read
       with open(t.anno_file, 'r') as f:
         anno = json.load(f)
-        # dummy info -> do nothing, later
-        # category check
-        if bool(common_cat):
+
+        # 0) category check
+        if not bool(common_cat):
           common_cat = anno['categories']
-        catmap = # TODO
-        # licneses stacking
-        # image mapping
+        # do comparision, return anno's cat's id -> common cat's id dict
+        cat_id_map, task_mapping_info['cat_id_map'] = categories_cmp(common_cat, anno['categories'])
+        if cat_id_map == None:
+          print("* categories_cmp ends with error *")
+          print("** aborted **")
+          exit(0)
+        print('[debug] cat_id_map\n', cat_id_map)
+        pdb.set_trace()
+        # make categories # do later at end of project loop
+
+        print('[per img] new_anno_json[\'images\']')
+        pdb.set_trace()
+        # 1) image copy
+        #    include copy & rename
+        l_img_id = 0
+        img_id_map = {}
         for img in anno['images']:
-          img['id']
-        # annotations remapping
+          # img file copy
+          org_name = img['file_name'].split('/')[1]
+          org_file = sp / img['file_name']
+          new_name = t.shortname + str(l_img_id)
+          new_file = tp / tp_pj_task_name / new_name
+          shutil.copy(org_file, new_file) #TODO 20210803 #org_file location wrong #TODO hotfix!!
+          _info_str = org_file + '->' + new_file
+          task_mapping_info['img_id_map'].append(_info_str)
+
+          print('[debug] '+_info_str)
+          pdb.set_trace()
+          # id matching # will be used for annotations['image_id']
+          org_id = img['id']
+          new_id = g_img_id
+          img_id_map[org_id] = new_id
+          new_anno_json['images'].append({ "id": g_img_id,
+                                          "width": img["width"],
+                                          "height": img["height"],
+                                          "license": img["license"],
+                                          "file_name": tp_pj_task_name+'/'+new_name,
+                                          "flickr_url": img["flickr_url"],
+                                          "coco_url": img["coco_url"],
+                                          "date_created": img["date_created"]
+                                          })
+          g_img_id += 1
+          l_img_id += 1
+
+        print('[per anno] new_anno_json[\'annotations\']')
+        pdb.set_trace()
+        # 2) make annotations detail
+        for an in anno['annotations']:
+          new_anno_json['annotations'].append({"id": g_anno_id,
+                                               "image_id": img_id_map[an['image_id']],
+                                               "category_id": cat_id_map[an['category_id']],
+                                               "segmentation": an['segmentation'],
+                                               "area": an['area'],
+                                               "bbox": an['bbox'],
+                                               "iscrowd": an['iscrowd'],
+                                               "attributes": an['attributes']
+                                               })
+        pdb.set_trace()
+        # TODO(changmin): supercategory = ''
+
+    # end for task_list:
+  # end for tsr_table.plist:
+
+  # N) make other details of new annotation json file
+  # new_anno_json['images'] = done
+  # new_anno_json['annotations'] = done
+  new_anno_json['categories'] = common_cat
+  new_anno_json['licenses'] = [{"name":"",
+                                "id": 0,
+                                "url":""
+                                }]
+  new_anno_json['info'] = {"contributor":"",
+                           "date_created": datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S'),
+                           "description": "",
+                           "url": "",
+                           "version": "",
+                           "year": datetime.datetime.now().strftime('%Y')
+                           }
+  # TODO
+  # dummy info fill
+  # license stacking
 
   #make new tsr
   print("make new tsr")
@@ -246,21 +388,18 @@ if __name__ == "__main__":
   if args.command == "migrate":
     # option --sdir, tdir check
     if args.sdir == None:
-      ans = input("command \"migrate\" require --sdir. continue as default? [default=./results] [Y/n]: ")
-      if (ans == 'y') or (ans == 'Y') or (ans == ''):
+      if yntest("command \"migrate\" require --sdir. continue as default? [default=./results]", "[Y/n]"):
         args.sdir = "./results"
         print("* sdir = %s *" % args.sdir)
       else:
-        print("* sdir is not decided -> abort *")
+        print("** sdir is not decided -> abort **")
         exit(0)
     if args.tdir == None:
-      ans = input("command \"migrate\" require --tdir. continue as default? [default=./tsr] [Y/n]: ")
-
-      if (ans == 'y') or (ans == 'Y') or (ans == ''):
+      if yntest("command \"migrate\" require --tdir. continue as default? [default=./tsr]", "[Y/n]"):
         args.tdir = "./tsr"
         print("* tdir = %s *" % args.tdir)
       else:
-        print("* sdir is not decided -> abort *")
+        print("** tdir is not decided -> abort **")
         exit(0)
     ntsr = migrate(args.sdir, args.tdir)
     if ntsr == None:
