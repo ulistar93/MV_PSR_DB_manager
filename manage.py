d66cee22ef30b2b725cef0d74ce23c1cde89ad82
Changmin Yi <ycm93dg@gmail.com>
2021-08-13 18:34:12 +0900

    [feat] add command 'stat_only'
    save the statistic as json file
    and can load the exist json file too
    
    TODO - make a TSR class from read json dict

--- manage.py
+++ manage.py
@@ -6,7 +6,6 @@ import argparse
 #import pandas
 import pickle # vs json
 import json
-#import dill
 from pathlib import Path
 import datetime
 import re
@@ -52,15 +51,20 @@ def txtest(message, default):
       return ans.split(' ')
     redo_msg = 're: '
 
-class Task:
+class Task():
   def __init__(self, t_path):
     self.path = t_path
     self.name = t_path.name
     self.shortname, self.image_loc, self.image_files, self.anno_file = self.set_task_info(t_path)
+    self.num_image = len(self.image_files)
 
   def __repr__(self):
     return "{name: %s, shortname: %s, path: %s, # of images: %d, image_location: %s, anno_file: %s}" % (self.name,self.shortname,str(self.path),len(self.image_files),str(self.image_loc),str(self.anno_file))
     #return "name: %s\nshortname: %s\npath: %s\nimage_location: %s\n# of images: %d\nanno_file: %s" % (self.name,self.shortname,str(self.path),str(self.image_loc),len(self.image_files),str(self.anno_file))
+
+#  def __iter__(self):
+#    for key in self.__dict__:
+#      yield key, getattr(self, key)
 
   def set_task_info(self, path):
     # read coco format
@@ -82,6 +86,7 @@ class Task:
     image_format_support = ['jpg', 'jpeg', 'png', 'bmp']
     for img_fmt in image_format_support:
       _image_files = list(path.rglob('*.'+img_fmt))
+      _image_files.sort()
       for i in _image_files:
         if i.parent in _image_loc:
           pass
@@ -104,16 +109,20 @@ class Task:
     # in order to integrate between old(/images/*.jpg) and new dataset, to do this
     _name = re.sub('[^a-zA-Z0-9_/\-]', '', _name)
     _name = _name.replace('-','_').replace('/', '-')
-    #pdb.set_trace()
     return _name, _image_loc, _image_files_all, _anno_file
 
-class Project:
+class Project():
   def __init__(self, p_path):
     self.name = p_path.name
     self.task_list = [ Task(x) for x in list(p_path.iterdir()) if x.is_dir() ]
+    self.num_task = len(self.task_list)
 
   def __repr__(self):
-    return "{name: %s, len(task_list): %d}" % (self.name,len(self.task_list))
+    return "{name: %s, num_task: %d}" % (self.name,self.num_task)
+
+#  def __iter__(self):
+#    for key in self.__dict__:
+#      yield key, getattr(self, key)
 
 class TSR:
   def __init__(self, sdir):
@@ -122,8 +131,25 @@ class TSR:
     self.date_created = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M:%S')
     self.plist = self.set_project_list(sdir)
 
+  def read_dict(self, r_dic):
+    for key in r_dic:
+      if 'name' == key:
+        self.name = r_dic[key]
+        # TODO
+        # the function is a kind of copy constructor for json loading
+        # please refer the other comments by searching "json_decoder"
+      pass
+
   def __repr__(self):
     return "name: %s\ndate_created: %s\nlen(plist): %d" % (self.name,self.date_created,len(self.plist))
+
+  #def __dict__(self):
+  #  return {'name': self.name, 'path': self.path, 'date_created': self.date_created, 'plist': [ p.__dict__ for p in self.plist]}
+
+#  def __iter__(self):
+#    for key in self.__dict__:
+#      yield key, getattr(self, key)
+    #return {'name': self.name, 'path': self.path, 'date_created': self.date_created, 'plist': [ p.__dict__ for p in self.plist]}
 
   def set_project_list(self, sdir):
     _plist = []
@@ -133,37 +159,22 @@ class TSR:
     #pdb.set_trace()
     return _plist
 
-def override_dataset(tsr_old, tsr_new):
-  # copy tsr_new only if the destination file is not exist in tsr_old
-  # TODO
-  pass
+def json_encoder(o):
+  if isinstance(o, TSR):
+    return o.__dict__
+  elif isinstance(o, Project):
+    return o.__dict__
+    #return {'__Project__': o.__dict__}
+  elif isinstance(o, Task):
+    return o.__dict__
+  elif isinstance(o, Path):
+    return str(o)
+  raise TypeError('not JSON serializable')
 
-
-def integrate_dataset(tsr, tdir, override=False):
-  if override:
-    tsr_new = tsr
-    tsr_old = TSR(tdir)
-    override_dataset(tsr_old, tsr_new)
-  else:
-    tsr_new = migrate_dataset(tsr, tdir)
-  return tsr_new
-
-#TODO list
-def save_coco(self):
-  #TODO
-  pass
-
-def save_yolo(self):
-  #TODO
-  pass
-
-
-def exclude(self, keyword):
-  pass
-
-def make_db_table(_dir):
-  _tsr = TSR(_dir)
-  return _tsr
+#def json_decoder(o):
+#  if 'TSR' == o: ... TODO
+# to be set a proper constructors for each classes
+# refer the other comments which searched by "json_decoder"
 
 def migrate(s_dir, t_dir, extractor=None):
   #TODO extractor add
@@ -207,23 +218,26 @@ def migrate(s_dir, t_dir, extractor=None):
 
   #s_dir table file check
   print("[%s] s_dir table file check" % datetime.datetime.now().strftime('%H:%M:%S'))
-  st_pkl = Path(s_dir) / "db_table.pkl"
-  if st_pkl.exists():
+  #st_pkl = Path(s_dir) / "db_table.pkl"
+  st_json = Path(s_dir) / "db_table.json"
+  #if st_pkl.exists():
+  if st_json.exists():
     #s_dir table load
     print("* s_dir db_table.pkl file already exist -> load db_table.pkl *")
-    with open(st_pkl, 'rb') as f:
-      tsr_table = pickle.load(f) # load TSR class
-      #tsr_table = json.load(f) # load TSR class
-      #tsr_table = dill.load(f) # load TSR class
+    #with open(st_pkl, 'rb') as f:
+    with open(st_json, 'r') as f:
+      #tsr_table = pickle.load(f) # load TSR class
+      tsr_table = json.load(f) # load TSR class
   else:
     #s_dir table create
-    print("* s_dir db_table.pkl file is not exist -> make new TSR & save pkl")
+    #print("* s_dir db_table.pkl file is not exist -> make new TSR & save pkl")
+    print("* s_dir db_table.json file is not exist -> make new TSR & save json")
     tsr_table = TSR(Path(s_dir))
     #s_dir table save
-    with open(st_pkl, 'wb') as f:
-      pickle.dump(tsr_table, f) # save TSR class
-      #json.dump(tsr_table, f, indent=4) # save TSR class # TODO - to be serialized
-      #dill.dump(tsr_table, f) # save TSR class
+    #with open(st_pkl, 'wb') as f:
+    with open(st_json, 'w') as f:
+      #pickle.dump(tsr_table, f) # save TSR class
+      json.dump(tsr_table, f, indent=4) # save TSR class # TODO - to be serialized
 
   # copy and generate t_dir dataset #
   # TODO(changmin) : making as a function of some class object would be botter...
@@ -455,7 +469,7 @@ def excluder(elem):
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser(description="TSR db manager")
-  parser.add_argument('command', choices=['migrate', 'update', 'extract'],metavar='command', type=str, help="Among \"migrate, update, extract\", choose command what you want to do")
+  parser.add_argument('command', choices=['migrate', 'update', 'extract', 'stat_only'],metavar='command', type=str, help="Among \"migrate, update, extract, stat_only\", choose command what you want to do")
   parser.add_argument('-s','--sdir', metavar='PATH', type=str, help="source directory")
   parser.add_argument('-t','--tdir', metavar='PATH', type=str, help="target directory")
   parser.add_argument('-i','--include', metavar='LABEL', type=str, nargs='+', help="including conditions for filtering. divide as space")
@@ -525,7 +539,66 @@ if __name__ == "__main__":
     if ntsr == None:
       print("** the migration ended abnormally, please refer the error code **")
       exit(0)
-    # TODO adv extractor - handel label and attribute too
+    # TODO adv extractor - handle label and attribute too
     #                      suggest) read selection file (ex. --extract-file stop_sign.config) composed to some formatted context
+  elif args.command == "stat_only":
+    # option --sdir only
+    if args.sdir == None:
+      if yntest("* command \"stat_only\" require --sdir. continue as default? [default=./tsr] *", "[Y/n]"):
+        args.sdir = "./tsr"
+        print("-> sdir = %s" % args.sdir)
+      else:
+        print("** sdir is not decided -> abort **")
+        exit(0)
+
+    # json exist test
+    st_json = Path(args.sdir) / "db_table.json"
+    if st_json.exists():
+      # s_dir table load
+      with open(st_json, 'r') as f:
+        tsr_table_old = json.load(f)
+        # TODO : json_decoder
+        #tsr_table_old = json.load(f, object_hook = json_decoder)
+        # This json loading without object_hook(json_decoder),
+        # just load the data from json and make a "dict" type
+        # In order to get a whole TSR class type object,
+        # the each classes (TSR, Project, Task) should be implemented
+        # with some constructor which can get the data from dictionary
+      #print(tsr_table_old)
+      # Because of above issue, the printing is dirty
+      # we cannot use the __repr__ functions of each classes
+      # Temporary print below
+      for k in tsr_table_old:
+        if 'plist' in k:
+          print(k)
+          for p in tsr_table_old[k]:
+            for k1 in p:
+              if 'task_list' in k1:
+                print('\t',k1)
+                for t in p[k1]:
+                  for k2 in t:
+                    if 'image_files' in k2:
+                      print('\t\t',k2)
+                    else:
+                      print('\t\t',k2,t[k2])
+              else:
+                print('\t',k1,':',p[k1])
+        else:
+          print(k,':',tsr_table_old[k])
+
+      pdb.set_trace()
+      # ask whether renew the table json
+      if yntest("* s_dir db_table.json file already exist. Do you want to renew the db_table.json? *", "[y/N]"):
+        pass
+      else:
+        print("** s_dir db_table.json already exist -> abort **")
+        exit(0)
+    #s_dir table create
+    #print("* s_dir db_table.pkl file is not exist -> make new TSR & save pkl")
+    tsr_table = TSR(Path(args.sdir))
+    #s_dir table save
+    with open(st_json, 'w') as f:
+      pdb.set_trace()
+      json.dump(tsr_table, f, indent=4, default=json_encoder, ensure_ascii=False)
   else:
     args.__repr__()
